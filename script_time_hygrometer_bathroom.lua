040629dd88e308af36685191713458d2bc9ca8ac
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-03-02 19:22:01 +0100

    Update script_time_hygrometer_bathroom.lua

--- script_time_hygrometer_bathroom.lua
+++ script_time_hygrometer_bathroom.lua
@@ -5,16 +5,16 @@
 commandArray = {}
 
 -- devices
-local DEVICE_HYGRO = 'NameOfHumiditySensor'
-local DEVICE_FAN = 'NameOfExhaustFAN'
+local DEVICE_HYGROMETER = 'NameOfHumiditySensor'
+local DEVICE_SWITCH = 'NameOfExhaustFANSwitch'
 
 -- constant
-HYGRO_LEVEL_ON = tonumber(uservariables['Hygro SdB Max'])
-HYGRO_LEVEL_OFF = tonumber(uservariables['Hygro SdB Min'])
+HYGRO_LEVEL_ON = tonumber(uservariables['Bathroom Humidity Max'])
+HYGRO_LEVEL_OFF = tonumber(uservariables['Bathroom Humidity Min'])
 
 -- read hygrometer
-hygro_level = otherdevices_humidity[DEVICE_HYGRO]
-print(DEVICE_HYGRO .. ' level is ' .. hygro_level .. '%')
+hygro_level = otherdevices_humidity[DEVICE_HYGROMETER]
+print(DEVICE_HYGROMETER .. ' level is ' .. hygro_level .. '%')
 
 -- check if the sensor is on or has some weird reading
 if (hygro_level == 0 or hygro_level == nil) then
@@ -23,15 +23,15 @@ if (hygro_level == 0 or hygro_level == nil) then
 end
 
 -- check if VMC should start
-if (otherdevices[DEVICE_FAN] == 'Off' and hygro_level >= HYGRO_LEVEL_ON) then
-  print('Switching ' .. DEVICE_FAN .. ' ON as humidity level reached ' .. hygro_level .. '%')
-  commandArray[DEVICE_FAN] = 'On'
+if (otherdevices[DEVICE_SWITCH] == 'Off' and hygro_level >= HYGRO_LEVEL_ON) then
+  print('Switching ' .. DEVICE_SWITCH .. ' ON as humidity level reached ' .. hygro_level .. '% [max set to ' .. HYGRO_LEVEL_ON .. '%]')
+  commandArray[DEVICE_SWITCH] = 'On'
 end
 
 -- check if VMC should stop
-if (otherdevices[DEVICE_FAN] == 'On' and hygro_level <= HYGRO_LEVEL_OFF) then
-  print('Switching ' .. DEVICE_FAN .. ' OFF as humidity level down to ' .. hygro_level .. '%')
-  commandArray[DEVICE_FAN] = 'Off'
+if (otherdevices[DEVICE_SWITCH] == 'On' and hygro_level <= HYGRO_LEVEL_OFF) then
+  print('Switching ' .. DEVICE_SWITCH .. ' OFF as humidity level down to ' .. hygro_level .. '% [min set to ' .. HYGRO_LEVEL_OFF .. '%]')
+  commandArray[DEVICE_SWITCH] = 'Off'
 end
 
 return commandArray
